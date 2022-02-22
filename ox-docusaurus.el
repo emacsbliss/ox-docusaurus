674708a3d807801ea8002d497a4121bc03148a64
emacsbliss <me@emacsbliss.com>
2022-02-21 20:15:50 -0500

    [Admonitions] add support

--- ox-docusaurus.el
+++ ox-docusaurus.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: markdown org-mode Docusaurus
 ;; Homepage: https://github.com/emacsbliss/ox-docusaurus
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "26"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -19,7 +19,7 @@
 ;;
 ;;; Code:
 
-(load "ox-blackfriday")
+(load! "ox-blackfriday")
 
 (require 'ox-blackfriday)
 
@@ -62,6 +62,39 @@ INFO is a plist holding export options."
           async subtreep visible-only nil nil (lambda () (text-mode)))
 )
 
+(defun org-docusaurus-special-block (special-block contents info)
+  "Transcode a SPECIAL-BLOCK element from Org to HTML.
+CONTENTS holds the contents of the block.
+
+INFO is a plist used as a communication channel.
+This function is adapted from `org-blackfriday-special-block'."
+  (let* ((block-type (org-element-property :type special-block))
+         (block-type-plist (org-element-property :type-plist special-block))
+         (contents (or (org-trim
+                          (if (plist-get block-type-plist :raw)
+                              ;; https://lists.gnu.org/r/emacs-orgmode/2022-01/msg00132.html
+                              (org-element-interpret-data (org-element-contents special-block))
+                            contents))
+                         "")))
+    (message "block-type: %s" block-type)
+
+      ;; Admonitions support
+      ;; https://docusaurus.io/docs/markdown-features/admonitions
+      (cond
+       ((string= block-type "note")
+          (format ":::note\n\n%s\n\n:::" contents))
+       ((string= block-type "tip")
+          (format ":::tip\n\n%s\n\n:::" contents))
+       ((string= block-type "info")
+          (format ":::info\n\n%s\n\n:::" contents))
+       ((string= block-type "caution")
+          (format ":::caution\n\n%s\n\n:::" contents))
+       ((string= block-type "danger")
+          (format ":::danger\n\n%s\n\n:::" contents))
+
+       (t (org-blackfriday-special-block special-block contents info)))
+))
+
 (org-export-define-derived-backend 'docusaurus 'blackfriday
   :menu-entry
   '(?D "Export to Docusaurus-compatible Markdown"
@@ -74,6 +107,7 @@ INFO is a plist holding export options."
               (org-docusaurus-export-as-md a s v)))))
 
   :filters-alist '((:filter-body . org-docusaurus-body-filter))
+  :translate-alist '((special-block . org-docusaurus-special-block))
 )
 
 (provide 'ox-docusaurus)
